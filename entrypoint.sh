c49a47b0e2f815e9b8d4f9079b4bac050f6f4ce6
treasuredataps <69876638+treasuredataps@users.noreply.github.com>
2020-08-18 17:03:54 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-ls
-
 digdag check
-digdag push $td_wf_project_name --project $github_path -r "$(date +%Y-%m-%dT%H:%M:%S%z)"-"$(git show --pretty=format:'%T' | head -n 1)" -e $td_wf_endpoint -X client.http.headers.authorization="TD1 $td_master_apikey"
+digdag push $td_wf_project_name -e $td_wf_endpoint -X client.http.headers.authorization="TD1 $td_master_apikey"
